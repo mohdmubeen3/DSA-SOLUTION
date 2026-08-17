0fdfbba14b2e477cbdedfc62171e4a0610c188c9
mohdmubeen3 <sheikhmubeen411@gmail.com>
2026-08-17 10:55:55 +0530

    Time: 17 ms (16.61%) | Memory: 46.6 MB (16.61%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        if(s.length() != t.length()) return false;
+
+        int n = s.length();
+        int m = t.length();
+
+        for(char ch : s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for(char ch : t.toCharArray()){
+            if(!map.containsKey(ch)) return false;
+
+            map.put(ch, map.get(ch) - 1);
+            if(map.get(ch) < 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
