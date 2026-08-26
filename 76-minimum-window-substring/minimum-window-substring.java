dd84cea77201f677c2a2e2275f9e8b30885804e6
mohdmubeen3 <sheikhmubeen411@gmail.com>
2026-08-26 12:38:43 +0530

    Time: 2 ms (99.80%) | Memory: 45.5 MB (82.43%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,50 +1,51 @@
-import java.util.*;
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character, Integer> map = new HashMap<>(); 
-        HashMap<Character,Integer> window = new HashMap<>();
+        int freq [] = new int[128];
+        int freq2 [] = new int[26];
+
         for(int i = 0; i<t.length(); i++){
-            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
+            freq[t.charAt(i)]++;
         }
-        int required = map.size();
-        int formed = 0;
+
         int l = 0;
-        int min = Integer.MAX_VALUE;
+        int r = 0;
         int start = 0;
-        for(int r = 0; r<s.length(); r++){
-            char c = s.charAt(r);
-            if(map.containsKey(c)){
-                window.put(c, window.getOrDefault(c, 0) + 1);
-                if(window.get(c).equals(map.get(c))){
-                    formed += 1;
-                }
+        int min = Integer.MAX_VALUE;
+        int rem = t.length();
+        while(r < s.length()){
+            if(freq[s.charAt(r)] > 0){
+                
 
-               
+                rem--;
+
+                
             }
-             while(formed == required){
-                   if(r - l + 1 < min){
-                    min = r - l + 1;
-                    start = l;
+            freq[s.charAt(r)]--;
 
-                   }
-                 char left = s.charAt(l);
-                 if (map.containsKey(left)) {
+            while(rem == 0){
 
-                  window.put(left, window.get(left) - 1);
-                   if (window.get(left) < map.get(left)) {
-                        formed--;
-                    }
-                    
-                 }
-                 l++;
-                }
-             
+                if(r - l + 1 < min){
+    min = r - l + 1;
+    start = l;
+}
 
+
+
+                freq[s.charAt(l)]++;
+                
+
+                if(freq[s.charAt(l)] > 0){
+                       rem++;
+               }
+                l++;
+
+
+
+            }
+            r++;
         }
-         if (min == Integer.MAX_VALUE)
-            return "";
 
-        return s.substring(start, start + min);
-
+        if(min == Integer.MAX_VALUE) return "";
+        else return s.substring(start, start + min);
     }
 }
