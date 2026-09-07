44116d611fefb49c3523858375ff5a8cf5a02531
mohdmubeen3 <sheikhmubeen411@gmail.com>
2026-09-07 12:05:48 +0530

    Time: 2 ms (91.80%) | Memory: 47.7 MB (57.33%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -4,14 +4,14 @@ class Solution {
         int j = 0;
         for(int i = 0; i<n; i++){
             if(nums[i] != 0) {
-                nums[j++] = nums[i];
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
             }
         }
 
-        while(j < n){
-            nums[j++] = 0;
-        }
-
+       
     
     }
 }
